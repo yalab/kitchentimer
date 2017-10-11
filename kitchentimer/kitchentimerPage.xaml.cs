3c205f68ebe142cb554dd266e6442a2812b06314
yalab <rudeboyjet@gmail.com>
2017-10-11 18:26:52 +0900

    First commit.

---
+++ kitchentimer/kitchentimerPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace kitchentimer
+{
+    public partial class kitchentimerPage : ContentPage
+    {
+        public kitchentimerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
