319579514e157082c0fd1045e8a6f429e6b80549
yalab <rudeboyjet@gmail.com>
2017-10-15 17:56:39 +0900

    Button tap

--- kitchentimer/kitchentimerPage.xaml.cs
+++ kitchentimer/kitchentimerPage.xaml.cs
@@ -1,12 +1,20 @@
 ﻿using Xamarin.Forms;
+using System;
 
 namespace kitchentimer
 {
     public partial class kitchentimerPage : ContentPage
     {
+        DateTime finishTime;
         public kitchentimerPage()
         {
             InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            finishTime = DateTime.Now.AddMinutes(1);
+            valueLabel.Text = finishTime.ToString("mm:ss");
         }
     }
 }
