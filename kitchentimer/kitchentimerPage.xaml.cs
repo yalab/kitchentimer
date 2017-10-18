77d26b015eb0560556558d525cf880a9c640aab1
yalab <rudeboyjet@gmail.com>
2017-10-18 18:59:34 +0900

    Implement Timer stop resume.

--- kitchentimer/kitchentimerPage.xaml.cs
+++ kitchentimer/kitchentimerPage.xaml.cs
@@ -1,41 +1,75 @@
 ﻿using Xamarin.Forms;
 using System;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace kitchentimer
 {
     public partial class kitchentimerPage : ContentPage
     {
         DateTime finishTime;
+        DateTime stoppedTime;
         IRingTone ringTone;
         DateTime TimeZero = new DateTime(0);
         int Seconds = 0;
+        const int MAX_SECONDS = 100 * 60;
+        const string START_LABEL = "Start";
 
         public kitchentimerPage()
         {
             InitializeComponent();
             ringTone = DependencyService.Get<IRingTone>();
-            finishTime = TimeZero;
-
+            ResetLabel();
             foreach (var s in Enum.GetValues(typeof(Sec)))
             {
                 var button = this.FindByName<Button>("button" + s.ToString());
-                button.Text = ((int)s / 60).ToString() + "分";
-                button.Clicked += ((object sender, EventArgs e) => OnButtonClick(s));
+                button.Text = TimeLabel((int)s);
+                button.Clicked += ((object sender, EventArgs e) => OnSecButtonClick(s));
             }
         }
 
-        void OnButtonClick(Object sec)
+        private void OnSecButtonClick(Object sec)
         {
             Seconds += (int) sec;
-            var m = Seconds / 60;
-            var s = Seconds - m * 60;
-            valueLabel.Text = string.Format("{0:00}:{1:00}", m, s);
+            if (Seconds > MAX_SECONDS){
+                Seconds = MAX_SECONDS;
+            }
+            valueLabel.Text = FormatMinSec(Seconds);
+        }
+
+        string TimeLabel(int sec)
+        {
+            var m = sec / 60;
+            var s = sec - m * 60;
+            StringBuilder label = new StringBuilder();
+            if (m > 0)
+            {
+                label.Append(m);
+                label.Append("分");
+            }
+            if (s > 0)
+            {
+                label.Append(s);
+                label.Append("秒");
+            }
+            return label.ToString();
+        }
+
+        private string FormatMinSec(int sec)
+        {
+            var m = sec / 60;
+            var s = sec - m * 60;
+            return string.Format("{0:00}:{1:00}", m, s);
         }
 
         private void OnStartButtonClicked(object sender, EventArgs args)
         {
-            StartTimer();
+            Button btn = (Button)sender;
+            if (Seconds > 0 || stoppedTime > TimeZero) {
+                StartTimer();
+            } else {
+                StopTimer();
+            }
         }
 
         private void OnResetButtonClicked(object sender, EventArgs args)
@@ -45,18 +79,27 @@ namespace kitchentimer
 
         private async void StartTimer()
         {
-            if(Seconds < 1)
-            {
-                return;
+            startButton.Text = "Stop";
+            if (stoppedTime > TimeZero) {
+                finishTime += DateTime.Now - stoppedTime;
+                stoppedTime = TimeZero;
+            } else {
+                finishTime = DateTime.Now.AddSeconds(Seconds);
+                Seconds = 0;
             }
-            finishTime = DateTime.Now.AddSeconds(Seconds);
             while (true)
             {
-                if(!Tick()){
+                if (!Tick()) {
                     break;
                 }
                 await Task.Delay(100);
             }
+        }
+
+        private void StopTimer()
+        {
+            startButton.Text = "Start";
+            stoppedTime = DateTime.Now;
         }
 
         private void ResetTimer()
@@ -68,16 +111,19 @@ namespace kitchentimer
 
         private Boolean Tick()
         {
-            if(finishTime == TimeZero){
+            if (finishTime == TimeZero) {
                 return false;
+            }
+            if (stoppedTime > TimeZero) {
+                return true;
             }
             DateTime now = DateTime.Now;
             TimeSpan diff = finishTime - now;
-            if (diff <= TimeSpan.Zero){
+            if (diff <= TimeSpan.Zero) {
                 TimerFinished();
                 return false;
             }
-            valueLabel.Text = diff.ToString(@"mm\:ss");
+            valueLabel.Text = FormatMinSec((int)diff.TotalSeconds);
             return true;
         }
 
@@ -85,10 +131,14 @@ namespace kitchentimer
         {
             valueLabel.Text = "--:--";
             Seconds = 0;
+            stoppedTime = TimeZero;
+            finishTime = TimeZero;
+            startButton.Text = "Start";
         }
 
         private void TimerFinished ()
         {
+            finishTime = TimeZero;
             ringTone.Play();
         }
     }
