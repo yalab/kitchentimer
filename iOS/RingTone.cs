c8053e655c004efe615bbcfa691da828f07bdde4
yalab <rudeboyjet@gmail.com>
2017-11-06 18:42:34 +0900

    ios RingTone

---
+++ iOS/RingTone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using AudioToolbox;
+
+[assembly: Dependency(typeof(kitchentimer.iOS.RingTone))]
+namespace kitchentimer.iOS
+{
+    public class RingTone : IRingTone
+    {
+        private uint alermSoundId = 1005;
+
+        public void Play()
+        {
+            var s = new SystemSound(alermSoundId);
+            s.PlaySystemSound();
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
